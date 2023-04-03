1a558fb07d8952c5f7094fda3e6673b5bb272298
XploR5 <brohan5501@gmail.com>
2023-04-03 05:56:13 -0400

    seperated functionality

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,12 +1,27 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+// use std::env;
+use std::thread;
+// use rayon::iter;
+// use std::sync::Arc;
+// use async_std::task;
+// use std::sync::Mutex;
+// use rayon::prelude::*;
+// use std::cell::RefCell;
+// use futures::stream::iter;
+// use tokio::runtime::Runtime;
+// use sqlx::postgres::PgPoolOptions;
+// use sqlx::{Executor, Postgres, Transaction};
+
+use actix_web::{
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
 use chrono::{DateTime, Utc};
-use futures::stream::iter;
+use postgres::Transaction;
+use std::mem::replace;
+use async_std::task;
 use rand::Rng;
-use rayon::iter;
-use sqlx::postgres::{PgConnectOptions, PgPool};
-use sqlx::{Error, Executor, Postgres, Transaction};
-use std::cell::RefCell;
-use std::sync::Mutex;
+use sqlx::{postgres::{PgConnectOptions, PgPool}, pool, Postgres};
+use sqlx::Error;
 use std::time::{Duration, Instant};
 
 #[derive(Debug, serde::Deserialize)]
@@ -27,135 +42,16 @@ struct CreateDataResponse {
     programlang: String,
 }
 
-async fn insert_data(pool: &PgPool, data: &[(i32, String, i32, i32)]) -> Result<usize, sqlx::Error> {
-    let mut tx = pool.begin().await?;
-    let mut count = 0;
 
-    for row in data {
-        let createdat = &row.1; // createdat is a String
-        let result: Result<_, sqlx::Error> = sqlx::query(
-            "INSERT INTO single(plant_id, createdat, quality, performance) VALUES ($1, $2, $3, $4)",
-        )
-        .bind(&row.0)
-        .bind(createdat)
-        .bind(&row.2)
-        .bind(&row.3)
-        .execute(&mut tx)
-        .await;
-
-        if result.is_err() {
-            let _ = tx.rollback().await;
-            return Err(result.unwrap_err());
-        } else {
-            count += 1;
-        }
-    }
-
-    tx.commit().await?;
-    Ok(count)
-}
-
-// new code
-async fn create_data_for_plant(
-    pool: web::Data<PgPool>,
-    req: web::Json<CreateDataRequest>,
-) -> impl Responder {
-    let CreateDataRequest {
-        plant_id,
-        start_date,
-        end_date,
-        interval,
-    } = req.into_inner();
-
-    let start_time = Instant::now(); // start timer
-  
-    let mut list = Vec::new();
-    let mut rng = rand::thread_rng();
-    let start_datetime = DateTime::parse_from_rfc3339(&start_date).unwrap().with_timezone(&Utc);
-    let end_datetime = DateTime::parse_from_rfc3339(&end_date).unwrap().with_timezone(&Utc);
-    let interval_duration = chrono::Duration::from_std(Duration::from_secs(interval.try_into().unwrap())).unwrap();
-
-    let mut handles = Vec::new();
-    for plant in plant_id {
-        let pool_clone = pool.clone();
-        let mut list_clone = list.clone();
-        let handle = tokio::task::spawn(async move {
-            let mut current_datetime = start_datetime;
-            while current_datetime <= end_datetime {
-                let obj = (
-                    plant,
-                    current_datetime.to_rfc3339(), // convert to string
-                    fastrand::Rng::new().i32(1..=100),
-                    fastrand::Rng::new().i32(1..=100),
-                );
-                list_clone.push(obj);
-                current_datetime = current_datetime + interval_duration;
-            }
-            let mut tx = pool_clone.begin().await.unwrap();
-            let count = insert_data(&pool_clone, &list_clone).await.unwrap();
-            if let Err(_) = tx.commit().await {
-                return 0;
-            }
-            count
-        });
-        handles.push(handle);
-    }
-
-    let mut count = 0;
-    for handle in handles {
-        count += handle.await.unwrap();
-    }
-
-    print!("Total duration is {} ns", start_time.elapsed().as_millis());
-    let totalduration = start_time.elapsed().as_millis();
+ async fn create_and_insert_data(plant: i32, start_datetime: DateTime<Utc>, end_datetime: DateTime<Utc>, interval_duration:  chrono::Duration, pool: web::Data<PgPool>){
+    print!("plant: {}  wit st_dtc: {}  end: {} dur: {}\n", plant, start_datetime, end_datetime, interval_duration);
     
-    let createduration = totalduration ;
-    print!("Created the obj list in {} ns per plant", createduration);
-
-    let insertduration = totalduration - createduration ;
-    print!("Time for insertion into db -> {} ns per plant", insertduration);
-
-    let programlang = format!("Rust");
-    
-    HttpResponse::Created().json(CreateDataResponse {
-        data: "Data Added successfully".to_string(),
-        count,
-        createduration,
-        insertduration,
-        totalduration,
-        programlang,
-    })
-}
-
-
-// new code
-
-
-
-//////// OLD code, keep safe
-
-/*
-async fn create_data_for_plant(
-    pool: web::Data<PgPool>,
-    req: web::Json<CreateDataRequest>,
-) -> impl Responder {
-    let CreateDataRequest {
-        plant_id,
-        start_date,
-        end_date,
-        interval,
-    } = req.into_inner();
-
-    let start_time = Instant::now(); // start timer
-  
-    let mut list = Vec::new();
+    let mut tx = pool.begin().await.unwrap(); // new
     let mut rng = rand::thread_rng();
-    let start_datetime = DateTime::parse_from_rfc3339(&start_date).unwrap().with_timezone(&Utc);
-    let end_datetime = DateTime::parse_from_rfc3339(&end_date).unwrap().with_timezone(&Utc);
-    let interval_duration = chrono::Duration::from_std(Duration::from_secs(interval.try_into().unwrap())).unwrap();
+    let mut list = Vec::new();
+
 
     // This loop creates and stores data in a vector
-    for plant in plant_id {
     let mut current_datetime = start_datetime;
     while current_datetime <= end_datetime {
         let obj = (
@@ -166,20 +62,11 @@ async fn create_data_for_plant(
         );
         list.push(obj);
         current_datetime = current_datetime + interval_duration;
-        }
     }
 
-    let createduration = start_time.elapsed().as_millis(); // stop timer
-    print!("Created the obj list in {} ns", createduration);
-
-
-    let mut tx = pool.begin().await.unwrap();
-    let mut count = 0;
-
-    // This loop takes data from the vector and stores it in the postgreSQL database
-    /*
+    // This loop takes data from the vector with name list and stores it in the postgreSQL database
     for row in &list {
-        let createdat = &row.1; // createdat is a String
+        let createdat = &row.1; // createdate is a String
         let result: Result<_, Error> = sqlx::query(
             "INSERT INTO single(plant_id, createdat, quality, performance) VALUES ($1, $2, $3, $4)",
         )
@@ -189,46 +76,81 @@ async fn create_data_for_plant(
         .bind(&row.3)
         .execute(&mut tx)
         .await;
-
         if result.is_err() {
-            let _ = tx.rollback().await;
-            return HttpResponse::InternalServerError().finish();
-        } else {
-            count += 1;
+            // let _ = &tx.rollback().await;
         }
     }
+   let res = tx.commit().await;
+    print!("The err is: {:#?}", res);
+    // Ok(())
 
-     */
-
-//////  new start
-
-// /* 
-    let pool_clone = pool.clone();
-    let insert_task = tokio::task::spawn_blocking(move || {
-        let runtime = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .unwrap();
-        runtime.block_on(insert_data(&pool_clone, &list))
-    });
+}
 
 
-    let insert_result = insert_task.await.unwrap();
-    count += insert_result.unwrap();
-// */
+async fn create_data_for_plant(
+    pool: web::Data<PgPool>,
+    req: web::Json<CreateDataRequest>,
+) -> impl Responder {
+    let CreateDataRequest {
+        plant_id,
+        start_date,
+        end_date,
+        interval,
+    } = req.into_inner();
 
-///// new end
+    let start_time = Instant::now(); // start timer
+    let start_datetime = DateTime::parse_from_rfc3339(&start_date)
+        .unwrap()
+        .with_timezone(&Utc);
+    let end_datetime = DateTime::parse_from_rfc3339(&end_date)
+        .unwrap()
+        .with_timezone(&Utc);
+    let interval_duration =
+        chrono::Duration::from_std(Duration::from_secs(interval.try_into().unwrap())).unwrap();
+
+
+// // focus here 
+
+    for plant in plant_id {    
+           create_and_insert_data(plant.clone(), start_datetime, end_datetime, interval_duration, pool.clone()).await;
+    }
+
+
+    // for plant in plant_id {    
+    //     let cloned_plant = plant.clone();
+    //     let cloned_pool = pool.clone();
+    //     let cloned_start = start_datetime.clone();
+    //     let cloned_end = end_datetime.clone();
+    //     let cloned_duration = interval_duration.clone();
+    
+    //     task::spawn(async move {
+    //         create_and_insert_data(cloned_plant, cloned_start, cloned_end, cloned_duration, cloned_pool).await;
+    //     });
+    // }
+
+
+// // focus above
+
+
+
+
+    // */
+    let count = 0;
+
+    let createduration = start_time.elapsed().as_millis(); // stop timer
+    print!("Created the obj list in {} ns", createduration);
 
     print!("Total duration is {} ns", start_time.elapsed().as_millis());
     let totalduration = start_time.elapsed().as_millis();
-    
+
     let insertduration = totalduration - createduration; // stop timer
     print!("Time for insertion into db -> {}ns", insertduration);
 
     let programlang = format!("Rust");
-    if let Err(_) = tx.commit().await {
-        return HttpResponse::InternalServerError().finish();
-    }
+
+    // if let Err(_) = tx.commit().await {
+    //     return HttpResponse::InternalServerError().finish();
+    // }
 
     HttpResponse::Created().json(CreateDataResponse {
         data: "Data Added successfully".to_string(),
@@ -237,10 +159,8 @@ async fn create_data_for_plant(
         insertduration,
         totalduration,
         programlang,
-    })  
+    })
 }
-
-*/
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -256,7 +176,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-            .data(pool.clone())
+            .app_data(Data::new(pool.clone()))
             .service(web::resource("/createdata").route(web::post().to(create_data_for_plant)))
     })
     .bind("127.0.0.1:3000")?
